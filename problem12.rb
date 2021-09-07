49f981c5e7760ed0dd9793d458f6212e34e3b3b4
YOUR NAME <jhanov@gmail.com>
2021-09-06 21:23:13 -0500

    add soluion to problem 5

--- problem12.rb
+++ problem12.rb
@@ -1,28 +1,5 @@
-triangle_numbers = [1, 3, 6, 10]
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
-i = 0
-while i < 50000
-  sum = 0
-  starting_num = triangle_numbers.length + 1
-  while starting_num > 0
-    sum = starting_num + sum
-    starting_num = starting_num - 1
-  end
-  i = i + 1
-  triangle_numbers << sum
-end
+count = 10
 
-# find the divisers
-
-i_2 = 0
-divisors_array = []
-highest_number = 0
-while i_2 < triangle_numbers.length
-  divisors_array = (1..triangle_numbers[i_2]).select { |n| triangle_numbers[i_2] % n == 0 }
-  # find divisors array lenght keep track of a max.
-  if divisors_array.length + 1 > highest_number
-    highest_number = divisors_array.length + 1
-    p highest_number
-  end
-  i_2 = i_2 + 1
-end
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
